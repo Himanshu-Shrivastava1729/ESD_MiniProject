48bf0fffc38840a092d650ba71db4efdc4d587fe
himanshu <himanshushrivastava227@gmail.com>
2024-11-20 20:29:06 +0530

    added employee login and token generation

--- ESD_Final_Project/src/main/java/com/himanshu/esd_final_project/controller/EmployeeController.java
+++ ESD_Final_Project/src/main/java/com/himanshu/esd_final_project/controller/EmployeeController.java
@@ -1,4 +1,34 @@
 package com.himanshu.esd_final_project.controller;
 
+
+import com.himanshu.esd_final_project.dto.EmployeeRequest;
+import com.himanshu.esd_final_project.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createEmployee(@RequestBody EmployeeRequest request)
+    {
+        boolean isCreated = employeeService.createEmployee(request);
+        String res;
+        if(isCreated)
+        {
+            res = "Employee created successfully";
+        }
+        else
+        {
+            res = "Employee creation failed";
+        }
+        return ResponseEntity.ok(res);
+    }
 }
--- ESD_Final_Project/src/main/java/com/himanshu/esd_final_project/service/EmployeeService.java
+++ ESD_Final_Project/src/main/java/com/himanshu/esd_final_project/service/EmployeeService.java
@@ -1,4 +1,46 @@
 package com.himanshu.esd_final_project.service;
 
+import com.himanshu.esd_final_project.dto.EmployeeRequest;
+import com.himanshu.esd_final_project.dto.LoginRequest;
+import com.himanshu.esd_final_project.entity.Employee;
+import com.himanshu.esd_final_project.helper.Encryption;
+import com.himanshu.esd_final_project.helper.JWTHelper;
+import com.himanshu.esd_final_project.mapper.EmployeeMapper;
+import com.himanshu.esd_final_project.mapper.StudentMapper;
+import com.himanshu.esd_final_project.repo.EmployeeRepo;
+import com.himanshu.esd_final_project.repo.StudentRepo;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
 public class EmployeeService {
+    private final EmployeeMapper employeeMapper;
+    private final EmployeeRepo employeeRepo;
+    private final Encryption encryption;
+    private final JWTHelper jWTHelper;
+
+    public boolean createEmployee(EmployeeRequest request) {
+        Employee employee = employeeMapper.toEmployee(request);
+        employee.setPassword(encryption.encode(employee.getPassword()));
+        employeeRepo.save(employee);
+        return true;
+    }
+
+    public Employee getEmployeeByEmail(String email) {
+        return employeeRepo.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Employee with email " + email + " not found"));
+    }
+
+    public String login(@Valid LoginRequest request) {
+        Employee employee = getEmployeeByEmail(request.email());
+        if(!encryption.validates(request.password(), employee.getPassword()))
+        {
+            return "Login failed";
+        }
+        return jWTHelper.generateToken(request.email());
+    }
 }
